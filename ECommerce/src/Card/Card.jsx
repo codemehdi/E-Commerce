b7a119ed93f3e9c1a4b35b8f243b933d326774df
unknown <code7.2an@outlook.com>
2024-10-22 19:23:20 +0100

    finished the cards, installed smooth-scrollbar package

--- ECommerce/src/Card/Card.jsx
+++ ECommerce/src/Card/Card.jsx
@@ -1,14 +1,29 @@
 import styles from './Card.module.css'
-import CustomerSupport from '../assets/customerIcon.png'
 
-function Card(){
+import {useSpring, animated} from "react-spring"
+
+function Number({ n }) {
+    const props = useSpring({
+        from: { number: 1 },
+        to: { number: n },
+        delay: 500,
+        config: { mass: 1, tension: 20, friction: 10 }
+    });
+
+    return <animated.div className={styles.NumberStat}>
+                {props.number.to(val => `+${val.toFixed(0)}`)}
+            </animated.div>;
+}
+
+function Card({Title, Image, number, altText}){
     return(
         <>
             <div className={styles.CardContainer}>
                 <div className={styles.imageCotainer}>
-                    <img src={CustomerSupport} alt="Icon" />
+                    <img src={Image} alt={altText} />
                 </div>
-                <h3 className={styles.Heading}>Satisfied Customers <br /> <span className={styles.HeadingNumber}>+1000</span></h3>
+                <h3 className={styles.Heading}>{Title}</h3>
+                <Number n = {number} />
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed laboriosam aliquid pariatur sequi, esse quia perspiciatis laborum odit molestiae sapiente?</p>
             </div>
             {/* "Fast Delivery," "Best Prices," "Customer Support" */}
--- ECommerce/src/Cards/Cards.jsx
+++ ECommerce/src/Cards/Cards.jsx
@@ -1,13 +1,31 @@
 import Card from '../Card/Card.jsx'
 import styles from './Cards.module.css'
+import CustomerSupport from '../assets/customerIcon.png'
+import DelieveryIcon from '../assets/DelieveryIcon.png'
+import PriceIcon from '../assets/PriceIcon.png'
 
 function Cards(){
     return(
         <>
             <div className={styles.CardsContainer}>
-                <Card />
-                <Card />
-                <Card />
+                <Card 
+                    Title= "Customer Support"
+                    Image= {CustomerSupport}
+                    altText = "CustomerSupport Icon"
+                    number = {2000}
+                />
+                <Card 
+                    Title= "Dicsounted Items"
+                    Image= {PriceIcon}
+                    altText = "Discount Icon"
+                    number = {50}
+                />
+                <Card 
+                    Title= "Delieverd Items"
+                    Image= {DelieveryIcon}
+                    altText = "Delievery Icon"
+                    number = {30000}
+                />
             </div>
         </>
     )
